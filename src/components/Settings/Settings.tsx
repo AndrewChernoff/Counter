c38b929f7202ca3e49ed0776aa9b69f2d561da5d
Andrew Chernoff <whitedrew538@gmail.com>
2023-01-06 02:03:01 +0500

    disabled btn in Settings.tsx

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -15,7 +15,7 @@ const Settings = ({onSetClick, setValues} : SettingsProps) => {
 
     let [max, setMax] = useState<number>(Number(localStorage.getItem('maxSettings')) || 5);
     let [min, setMin] = useState<number>(Number(localStorage.getItem('minSettings')) || 0);
-    const [error, setError] = useState<boolean>(false);
+    const [error, setError] = useState<boolean>(false); ///make button disabled
 
     useEffect(() => {
         let maxValue = localStorage.getItem('maxSettings'); 
@@ -44,7 +44,17 @@ const Settings = ({onSetClick, setValues} : SettingsProps) => {
     const onMinChange = (e: any) => {
         setMin(Number(e.currentTarget.value))
         localStorage.setItem('minSettings', JSON.stringify(min));
+    }
 
+    
+    let disabled = max < 0 || min < 0 || max < min || max === min 
+
+    const isDisabled = () => {
+        if (max < 0 || min < 0 || max < min || max === min) {
+            return true
+        } else {
+            return false
+        }
     }
 
     const isErrorForMin = min < 0 || max < min || max === min ? s.errorInput : s.input
@@ -58,7 +68,7 @@ const Settings = ({onSetClick, setValues} : SettingsProps) => {
          <input className={isErrorForMin} value={min} onChange={onMinChange} type={'number'} />
         <div className={s.settingsBtn}>
             
-        <Button title={'set'} isDisabled={error} callback={() => {
+        <Button title={'set'} isDisabled={isDisabled()} callback={() => {
             setValues(max, min)
             onSetClick()
         }} />
